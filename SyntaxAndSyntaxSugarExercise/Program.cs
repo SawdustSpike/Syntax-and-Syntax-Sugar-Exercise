467a4eaf3c2f33883604d29c58d45610f62059e3
Mike Cowell <spike@spikeandbetty.com>
2022-10-18 13:56:05 -0400

    init commit

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*Original code
+              int answer = 4;
+string response;
+if (answer < 9) 
+{
+    response = answer + " is less than nine";
+}
+else
+{
+    response = answer + " is greater than or equal to nine";
+}*/
+            //---------Inferred typing, string interpolation and ternary operator--------
+            var answer = 4;
+              var response = answer < 9 ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
+
         }
     }
 }
